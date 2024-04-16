9ad66e6cb145a363be1edf88e5f64f830acbdb51
PHP <hoaiphat990106@gmail.com>
2024-04-16 22:07:30 +0700

    add eureka-client in services

--- order-service/src/main/java/com/example/orderservice/client/InventoryServiceClient.java
+++ order-service/src/main/java/com/example/orderservice/client/InventoryServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8082")
+@FeignClient(name = "inventory-service")
 public interface InventoryServiceClient {
 
     @GetMapping(value = {"/api/inventory/getCodesValid"})
